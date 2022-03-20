3a1347d90cc84230729fe5d7655108af30909013
Bao Phung Gia <bao.phung@onemount.com>
2022-03-20 10:26:02 +0700

    remove css folder

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+// Set public assets path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+// Configurations
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
